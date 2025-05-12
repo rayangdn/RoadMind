e35599cf0bdc19202e2233a2f87ec0c42341379b
rayangdn <rayan.gauderon@epfl.ch>
2025-05-12 18:26:07 +0200

    added new hyperparameters testing

--- get_hype.py
+++ get_hype.py
@@ -6,25 +6,28 @@ def objective(trial):
     
     model, best_model_val_ade = train(
         num_epochs=100,
-        lr=trial.suggest_loguniform("lr", 1e-5, 1e-3, log=True),
-        weight_decay=trial.suggest_loguniform("weight_decay", 1e-6, 1e-3, log=True),
+        lr=trial.suggest_loguniform("lr", 1e-5, 1e-3),
+        weight_decay=trial.suggest_loguniform("weight_decay", 1e-6, 1e-3),
         scheduler_factor=trial.suggest_float("scheduler_factor", 0.5, 0.9),
         scheduler_patience=5,
         precision='high',
         hidden_dim=trial.suggest_int("hidden_dim", 64, 256),
         image_embed_dim=trial.suggest_int("image_embed_dim", 128, 256),
-        num_layers_gru=trial.suggest_int("num_layers_gru", 1,23),
+        num_layers_gru=trial.suggest_int("num_layers_gru", 1, 2),
         dropout_rate=trial.suggest_float("dropout_rate", 0.1, 0.7),
+        use_depth_aux=trial.suggest_categorical("use_depth_aux", [True, False]),
+        use_semantic_aux=trial.suggest_categorical("use_semantic_aux", [True, False]),
         include_heading=False,
         include_dynamics=True,
         batch_size=64,
+        logger_name='roadmind'
     )
     
     return best_model_val_ade
 
 def main():
     study = optuna.create_study(direction="minimize")
-    study.optimize(objective, n_trials=10)
+    study.optimize(objective, n_trials=20)
     
     print("Best hyperparameters: ", study.best_params)
     print("Best value: ", study.best_value)
